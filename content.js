599512d7ce6890f7fced11962866cafddb96162d
Dustin <JenyaYeskov>
2021-10-21 17:29:34 -0400

    changed some parts for filling operations bord instead of justice board

--- content.js
+++ content.js
@@ -1,156 +1,209 @@
 'use strict';
 
 chrome.runtime.onMessage.addListener(
-  async (request, sender,  sendResponse) => {
-    if( request.message === "clicked_browser_action" ) {
+    async (request, sender, sendResponse) => {
+        if (request.message === "clicked_browser_action") {
 
-      let TTElements = getTTElements();
+            let TTElements = getTTElements();
 
-      let TTTable = createTTTable(TTElements);
+            let TTTable = createTTTable(TTElements);
 
-      document.body.appendChild(TTTable);
+            document.body.appendChild(TTTable);
 
-      await navigator.clipboard.writeText(await getFormattedForTableStringFromArray(TTElements));
+            await navigator.clipboard.writeText(await getFormattedForTableStringFromArray(TTElements));
 
+        }
     }
-  }
 );
 
 
-async function getFormattedForTableStringFromArray(array){
+async function getFormattedForTableStringFromArray(array) {
 
-	let resultString = array[0];
+    let resultString = array[0];
 
-	for (let i = 1; i < array.length ; i++) {
+    for (let i = 1; i < array.length; i++) {
         resultString += String.fromCodePoint(9) + array[i];
     }
 
     return resultString;
 }
 
-function getTTElements(){
+function getTTElements() {
 
-	let loadBlockInfo = document.getElementsByClassName("load-block-info");
+    let loadBlockInfo = document.getElementsByClassName("load-block-info");
 
-	let loadNumber = getLoadNumber(loadBlockInfo);
+    let puDate = getPuDate(loadBlockInfo);
 
-	let addresses = getAddresses(loadBlockInfo);
+    let loadNumber = getLoadNumber(loadBlockInfo);
 
-	let puDate = getPuDate(loadBlockInfo);
+    let ref = getRef(loadBlockInfo);
 
-	let rate = getRate(loadBlockInfo);
+    let customer = getCustomer();
+
+    let addresses = getAddresses(loadBlockInfo);
+
+    let team = getTeam();
+
+    // let rate = getRate(loadBlockInfo);
 
 
-	let TTElements = [];
+    let TTElements = [];
 
     TTElements.push(puDate);
+    TTElements.push("");
     TTElements.push(loadNumber);
-    TTElements.push("Carolina Logistics");
+    TTElements.push(ref);
+
+    TTElements.push("");
+    // TTElements.push("");
+
+    TTElements.push(customer);
+    // TTElements.push("Carolina Logistics");
     TTElements.push(addresses);
-    TTElements.push(rate);
+	TTElements.push("");
+	TTElements.push("");
+	TTElements.push("");
+	TTElements.push("");
+	TTElements.push("");
+	TTElements.push("");
+	TTElements.push("");
+	TTElements.push("");
+	TTElements.push(team);
+    // TTElements.push(rate);
 
-	return TTElements;
+    return TTElements;
 }
 
-function getLoadNumber(loadBlockInfo){
+function getTeam() {
+    let dispatcher = getDispatcher();
 
-	let loadNumber = loadBlockInfo[0].querySelector("h2").textContent;
-
-	loadNumber = loadNumber.substring(7);	
-
-	return loadNumber;
+    if (dispatcher === "sync sync") {
+        return "Intercompany";
+    } else
+        return "";
 }
 
-function getAddresses(loadBlockInfo){
-
-	let pickUp = loadBlockInfo[1].getElementsByClassName("first-pick-up")[0];
-	pickUp = getAddress(pickUp);
-
-	let delivery = loadBlockInfo[1].getElementsByClassName("last-delivery")[0];
-	delivery = getAddress(delivery);
-
-	return pickUp + " - " + delivery;
+function getDispatcher() {
+    return getStrongSelectorFromTable(0).textContent;
 }
 
+function getRef(loadBlockInfo) {
 
-function getAddress(containingElement){
+    let ref = loadBlockInfo[0].querySelector("div.second-link").querySelector("strong");
 
-	let address = containingElement.querySelector("strong").textContent;
-
-	let counter = 0;
-	let position = 0;
-
-	// finding position of 2nd coma to cut string
-	while (counter !== 2) {
-
-		if ((position = address.indexOf(",", position)) !== -1) {
-			counter++;
-		}
-
-		position++;	
-	}
-
-
-	address = address.slice(0, position - 1);
-
-	return address;
+    return ref.textContent;
 }
 
+function getStrongSelectorFromTable(tableIndex) {
+    let customer = document.querySelectorAll(".table-responsive")[tableIndex];
+    customer = customer.querySelector("strong");
 
-function getPuDate(loadBlockInfo){
-
-	let date = 	loadBlockInfo[1].getElementsByClassName("first-pick-up")[0];
-
-	date = date.getElementsByClassName("styles_child__3uVE-")[0];
-
-	date = date.querySelector("div").textContent;
-
-	date = date.slice(0, date.indexOf(String.fromCodePoint(160)));
-
-	return date;
+    return customer;
 }
 
+function getCustomer() {
+    return getStrongSelectorFromTable(1).textContent;
+}
 
-function getRate(loadBlockInfo){
+function getLoadNumber(loadBlockInfo) {
 
-	let rate = loadBlockInfo[0].querySelectorAll("div");
+    let loadNumber = loadBlockInfo[0].querySelector("h2").textContent;
 
+    loadNumber = loadNumber.substring(7);
 
-	for (let i = 0; i < rate.length; i++) {
+    return loadNumber;
+}
 
-		if (rate[i].textContent) {
+function getAddresses(loadBlockInfo) {
 
-			if (rate[i].textContent.includes("Rate: ") ) {
-			
-				rate = rate[i].textContent.split(" ")[5];
+    let pickUp = loadBlockInfo[1].getElementsByClassName("first-pick-up")[0];
+    pickUp = getAddress(pickUp);
 
-				break;			
-			}
-		}
-	}
+    let delivery = loadBlockInfo[1].getElementsByClassName("last-delivery")[0];
+    delivery = getAddress(delivery);
 
-	return rate;
+    return pickUp + " - " + delivery;
 }
 
 
-function createTTTable(TTElements){
+function getAddress(containingElement) {
 
-      let div = document.createElement("div");
+    let address = containingElement.querySelector("strong").textContent;
 
-      let myTable = document.createElement("table");
+    let counter = 0;
+    let position = 0;
 
-      let myTr = document.createElement("tr");
+    // finding position of 2nd coma to cut string
+    while (counter !== 2) {
 
-      for (let i = 0; i < TTElements.length ; i++) {
+        if ((position = address.indexOf(",", position)) !== -1) {
+            counter++;
+        }
+
+        position++;
+    }
+
+
+    address = address.slice(0, position - 1);
+
+    return address;
+}
+
+
+function getPuDate(loadBlockInfo) {
+
+    let date = loadBlockInfo[1].getElementsByClassName("first-pick-up")[0];
+
+    date = date.getElementsByClassName("styles_child__3uVE-")[0];
+
+    date = date.querySelector("div").textContent;
+
+    date = date.slice(0, date.indexOf(String.fromCodePoint(160)));
+
+    return date;
+}
+
+
+function getRate(loadBlockInfo) {
+
+    let rate = loadBlockInfo[0].querySelectorAll("div");
+
+
+    for (let i = 0; i < rate.length; i++) {
+
+        if (rate[i].textContent) {
+
+            if (rate[i].textContent.includes("Rate: ")) {
+
+                rate = rate[i].textContent.split(" ")[5];
+
+                break;
+            }
+        }
+    }
+
+    return rate;
+}
+
+
+function createTTTable(TTElements) {
+
+    let div = document.createElement("div");
+
+    let myTable = document.createElement("table");
+
+    let myTr = document.createElement("tr");
+
+    for (let i = 0; i < TTElements.length; i++) {
         let th = document.createElement("th");
         th.innerHTML = TTElements[i];
 
-        myTr.appendChild(th); 
-      }
+        myTr.appendChild(th);
+    }
 
-      myTable.appendChild(myTr);
+    myTable.appendChild(myTr);
 
-      div.appendChild(myTable);
+    div.appendChild(myTable);
 
-      return div;
+    return div;
 }
